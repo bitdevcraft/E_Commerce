4b1229d966f246a3e1e5c18f869a469a0adc16eb
bitdevcraft <ryanvincent.capio@outlook.com>
2024-05-26 12:13:27 +0400

    Move the seed data as jsons

--- src/Persistence/Seed.cs
+++ src/Persistence/Seed.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using System.Text.Json;
 using Domain;
 using Microsoft.AspNetCore.Identity;
 
@@ -7,25 +9,12 @@ namespace Persistence
     {
         public static async Task SeedData(DataContext context, UserManager<AppUser> userManager)
         {
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
             if (!userManager.Users.Any())
             {
-                var users = new List<AppUser>
-                {
-                    new AppUser
-                    {
-                        FirstName = "Admin",
-                        LastName = "",
-                        UserName = "admin",
-                        Email = "admin@test.com"
-                    },
-                    new AppUser
-                    {
-                        FirstName = "User",
-                        LastName = "",
-                        UserName = "user",
-                        Email = "user@test.com"
-                    },
-                };
+                var tmpData = File.ReadAllText(path + @"/SeedData/users.json");
+                var users = JsonSerializer.Deserialize<List<AppUser>>(tmpData);
 
                 foreach (var user in users)
                 {
@@ -33,39 +22,40 @@ namespace Persistence
                 }
             }
 
-            if (context.Models.Any())
-                return;
-
-            var models = new List<Model>
+            if (!context.Models.Any())
             {
-                new Model { Title = "Model 1", Description = "Description Model 1" },
-                new Model { Title = "Model 2", Description = "Description Model 2" },
-                new Model { Title = "Model 3", Description = "Description Model 3" },
-            };
+                var tmpData = File.ReadAllText(path + @"/SeedData/models.json");
+                var models = JsonSerializer.Deserialize<List<Model>>(tmpData);
+                await context.Models.AddRangeAsync(models);
+            }
 
-            if (context.AppCompanies.Any())
-                return;
-            var company = new AppCompany
+
+            if (!context.AppCompanies.Any())
             {
-                Logo =
-                    "/Files/Uploaded/Images/vite.svg",
-                Name = "Default Company",
-                Email = "defaultcompany@test.com",
-                MobileNo = "000-000-000",
-                Hotline = "000-000-000",
-                About =
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                Vision =
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                Mission =
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                Address = "City, Country",
-            };
+                var tmpData = File.ReadAllText(path + @"/SeedData/company.json");
+                var company = JsonSerializer.Deserialize<AppCompany>(tmpData);
+                // var company = new AppCompany
+                // {
+                //     Logo =
+                //         "/Files/Uploaded/Images/vite.svg",
+                //     Name = "Default Company",
+                //     Email = "defaultcompany@test.com",
+                //     MobileNo = "000-000-000",
+                //     Hotline = "000-000-000",
+                //     About =
+                //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                //     Vision =
+                //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                //     Mission =
+                //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                //     Address = "City, Country",
+                // };
 
-            await context.AppCompanies.AddAsync(company);
+                await context.AppCompanies.AddAsync(company);
+            }
 
-            await context.Models.AddRangeAsync(models);
-            await context.SaveChangesAsync();
+
+            if (context.ChangeTracker.HasChanges()) await context.SaveChangesAsync();
         }
     }
 }
